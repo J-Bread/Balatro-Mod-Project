9cb42f9c58f1df80f1d32b00a9ff08403482ca2f
J-Bread <Joshuasano428@gmail.com>
2026-04-06 14:34:23 -0500

    Added art and code for Overwatch Clubs, Overwatch Diamonds, and Overwatch Hearts

--- main.lua
+++ main.lua
@@ -15,7 +15,7 @@ SMODS.Atlas {
     py = 32,
 }
 
--- ( Test, Clubs ) 
+-- ( Overwatch, Clubs ) 
 
 local atlas_OW_clubs_lc = SMODS.Atlas {
     key = "Overwatch_clubs_lc",
@@ -54,3 +54,124 @@ SMODS.DeckSkin {
         }
     },
 }
+
+-- ( Overwatch, Spades ) 
+
+local atlas_OW_spades_lc = SMODS.Atlas {
+    key = "Overwatch_spades_lc",
+    path = "Overwatch_Spades_lc.png",
+    px = 71,
+    py = 95,
+}
+local atlas_OW_spades_hc = SMODS.Atlas {
+    key = "Overwatch_spades_hc",
+    path = "Overwatch_Spades_hc.png",
+    px = 71,
+    py = 95,
+}
+
+-- Deck Skin
+SMODS.DeckSkin {
+    key = "OverwatchSpades",
+    suit = "Spades",
+    loc_txt = {
+        ['en-us'] = "Overwatch Spades"
+    },
+    palettes = {
+        {
+            key = 'lc',
+			ranks = {'Jack', 'Queen', "King"},
+            display_ranks = {"King", "Queen", "Jack"},
+            atlas = atlas_OW_spades_lc.key,
+            pos_style = "ranks"
+        },
+        {
+            key = 'hc',
+			ranks = {'Jack', 'Queen', "King",},
+            display_ranks = {"King", "Queen", "Jack"},
+            atlas = atlas_OW_spades_hc.key,
+            pos_style = "ranks"
+        }
+    },
+}
+
+-- ( Overwatch, Hearts ) 
+
+local atlas_OW_hearts_lc = SMODS.Atlas {
+    key = "Overwatch_hearts_lc",
+    path = "Overwatch_Hearts_lc.png",
+    px = 71,
+    py = 95,
+}
+local atlas_OW_hearts_hc = SMODS.Atlas {
+    key = "Overwatch_hearts_hc",
+    path = "Overwatch_Hearts_hc.png",
+    px = 71,
+    py = 95,
+}
+
+-- Deck Skin
+SMODS.DeckSkin {
+    key = "OverwatchHearts",
+    suit = "Hearts",
+    loc_txt = {
+        ['en-us'] = "Overwatch Hearts"
+    },
+    palettes = {
+        {
+            key = 'lc',
+			ranks = {'Jack', 'Queen', "King"},
+            display_ranks = {"King", "Queen", "Jack"},
+            atlas = atlas_OW_hearts_lc.key,
+            pos_style = "ranks"
+        },
+        {
+            key = 'hc',
+			ranks = {'Jack', 'Queen', "King",},
+            display_ranks = {"King", "Queen", "Jack"},
+            atlas = atlas_OW_hearts_hc.key,
+            pos_style = "ranks"
+        }
+    },
+}
+
+-- ( Overwatch, Diamonds ) 
+
+local atlas_OW_diamonds_lc = SMODS.Atlas {
+    key = "Overwatch_diamonds_lc",
+    path = "Overwatch_Diamonds_lc.png",
+    px = 71,
+    py = 95,
+}
+local atlas_OW_diamonds_hc = SMODS.Atlas {
+    key = "Overwatch_diamonds_hc",
+    path = "Overwatch_Diamonds_hc.png",
+    px = 71,
+    py = 95,
+}
+
+-- Deck Skin
+SMODS.DeckSkin {
+    key = "OverwatchDiamonds",
+    suit = "Diamonds",
+    loc_txt = {
+        ['en-us'] = "Overwatch Diamonds"
+    },
+    palettes = {
+        {
+            key = 'lc',
+			ranks = {'Jack', 'Queen', "King"},
+            display_ranks = {"King", "Queen", "Jack"},
+            atlas = atlas_OW_diamonds_lc.key,
+            pos_style = "ranks"
+        },
+        {
+            key = 'hc',
+			ranks = {'Jack', 'Queen', "King",},
+            display_ranks = {"King", "Queen", "Jack"},
+            atlas = atlas_OW_diamonds_hc.key,
+            pos_style = "ranks"
+        }
+    },
+}
+
